9eb57757fb67ee0ad6c6d4f8e8dadf3ccd3106d9
Núria Balaguer <nyxscream@gmail.com>
2018-12-05 17:41:14 +0100

    add proofOfWork function

--- dev/test.js
+++ dev/test.js
@@ -1,17 +1,4 @@
 const Blockchain = require('./blockchain');
-
 const ephemerum = new Blockchain;
 
-ephemerum.createNewBlock(49855, 'DSJF34U5RT349', '34095UB34345H');
-
-ephemerum.createNewTransaction(100, 'ALEXU3434HBF4DSFJN', 'JENIH34T3948GB89GV');
-
-ephemerum.createNewBlock(4920, 'LKDF94HTP4WIGH9W4', 'DNF483TGUBFG3498GT');
-
-ephemerum.createNewTransaction(50, 'ALEXU3434HBF4DSFJN', 'JENIH34T3948GB89GV');
-ephemerum.createNewTransaction(300, 'ALEXU3434HBF4DSFJN', 'JENIH34T3948GB89GV');
-ephemerum.createNewTransaction(2000, 'ALEXU3434HBF4DSFJN', 'JENIH34T3948GB89GV');
-
-ephemerum.createNewBlock(43467, 'DFN34058TH4GGH', '34098Y34509G3H4');
-
-console.log(ephemerum.chain[2]);
+console.log(ephemerum)
